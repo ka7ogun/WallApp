e66718be306a272b5a2a9f6e42505639e46634db
k_ogun <kogunka2@gmail.com>
2016-06-27 14:36:38 -0400

    Version4_b

--- portfolio_2/js/custom.js
+++ portfolio_2/js/custom.js
@@ -5,7 +5,7 @@ $(window).on("load", function(){
 		winW  = $(window).width();
 		winH  = $(window).height();
 	if(winW > 380){
-		$("#wrap").css("width",375);
+		$("#wrap").css("width", 375);
 	}
 	if(winH < 500){
 		$("#wrap").css("height",600);
@@ -16,9 +16,9 @@ $(window).on("load", function(){
 });
 
 
-$("#dropbtn").mouseout(function(){
-    $("#dropdown-content").hide();
-});
+// $("#dropbtn").mouseout(function(){
+//     $("#dropdown-content").hide();
+// });
 
 $("#dropbtn").click(function(){
     $("#dropdown-content").show();
